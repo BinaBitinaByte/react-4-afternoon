6e62718f35d5576e671d70dd56d941a2392697a0
binabitinabyte <matin.sabrina@gmail.com>
2019-04-26 10:06:13 -0700

    history-contact-works

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,15 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import ClassList from "./components/ClassList/ClassList";
 import Student from "./components/Student/Student";
+import History from "./components/History/History";
+import Contact from "./components/Contact/Contact";
 
 export default (
     <Switch>
         <Route component = {Home} exact path="/" />
         <Route component = {About} exact path="/about" />
+        <Route component = {History} exact path="/about/history" /> 
+        <Route component = {Contact} exact path="/about/contact" /> 
         <Route component = {ClassList} exact path="/classlist/:class" />
         <Route component = {Student} path= "/student/:id" />
     </Switch>
